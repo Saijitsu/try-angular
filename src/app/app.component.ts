18a593504422305de05aa3befcb7c036e7ee880c
Nicolas Turlet <35563943+Saijitsu@users.noreply.github.com>
2019-04-29 16:24:06 +0200

    create task class and try it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Task, TaskStatus } from './task';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +6,13 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'todo';
+  // title = 'todo';
+  task: Task = {
+    name: 'Learn Angular',
+    status: TaskStatus.Doing,
+    deadLine: new Date('2019-05-02')
+  };
 }
+
